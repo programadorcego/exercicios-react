93aed510e5f3cb953da2965a433080691083e94c
Programador Cego <programadorcego@gmail.com>
2023-09-19 15:56:00 -0300

    Exercício 12

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,24 @@ import {ChangeEvent, useState} from "react";
 export default function App(): JSX.Element
 {
 	const [text, setText] = useState<string>(() => "");
+	const [nomes, setNomes] = useState<string[]>(() => []);
+	
+	function addNome(): void
+	{
+		setNomes(nomes => [...nomes, text]);
+	}
 	
 	return (
 		<>
-			<h1>Exercício useState #11</h1>
-			
-			<p>{text}</p>
+			<h1>Exercício useState #12</h1>
 			
 			<input type="text" value={text} onChange={(e: ChangeEvent<HTMLInputElement>) => setText(() => e.target.value)} />
+			
+			<button onClick={addNome}>Adicionar</button>
+			
+			<ul>
+				{nomes.map((nome: string, index: number) => (<li key={index}>{nome}</li>))}
+			</ul>
 		</>
 	);
 }
